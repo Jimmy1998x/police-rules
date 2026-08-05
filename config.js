857292d7e46dd15ec794cdebcccb6c9f9d77d044
Jimmy1998x <kanpicha.rsm30@gmail.com>
2026-08-05 10:32:51 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+// ===============================
+// Police Rules Database Config
+// ===============================
+
+// Google Sheets (Published as Web)
+
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTbUpSwTQe49ncrZnVZfm270kIKR8FZmY13B3wltWJt2d9YOdcBeV5_gY8Vh0Ae0culpUHfBeNPrwjL/pubhtml?gid=0&single=true";
+
+// ชื่อคอลัมน์ใน Google Sheets
+
+const COLUMN = {
+
+    category: "หมวด",
+
+    title: "ชื่อคดี",
+
+    fine: "ค่าปรับ",
+
+    jail: "จำคุก",
+
+    note: "หมายเหตุ"
+
+};
